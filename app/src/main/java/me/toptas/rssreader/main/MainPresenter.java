d5d6aed78cc47980132bd8dc37e9c3946e2b799b
faruktoptas <faruktoptas@gmail.com>
2017-01-31 00:31:21 +0200

    Package name changed

--- app/src/main/java/me/toptas/rssreader/main/MainPresenter.java
+++ app/src/main/java/me/toptas/rssreader/main/MainPresenter.java
@@ -6,7 +6,7 @@ import me.toptas.rssreader.base.BasePresenter;
  * Created by faruktoptas on 28/01/17.
  */
 
-public class MainPresenter extends BasePresenter<MainContract.View> implements MainContract.Presenter {
+public class MainPresenter extends BasePresenter<me.toptas.rssreader.main.MainContract.View> implements me.toptas.rssreader.main.MainContract.Presenter {
 
     @Override
     public void loadRssFragments() {
